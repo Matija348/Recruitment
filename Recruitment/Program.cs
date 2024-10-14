37f1f684e45f5736ada64d899841d1d01efc2dd7
Matija Zezelj <matija.zezelj@elev.ga.ntig.se>
2024-10-14 09:51:31 +0200

    Måndags sista commit

--- Recruitment/Program.cs
+++ Recruitment/Program.cs
@@ -12,3 +12,26 @@
 ];
 
 List<string> lagen = [];
+
+while (lagen.Count < 3)
+{
+
+  Console.WriteLine("Möjliga rekryter");
+  for (int i = 0;i < lagen.Count;i++)
+  {
+    Console.WriteLine($"[{i}] {rekryter[i]}");
+  }
+   
+  Console.WriteLine("/n");
+
+
+
+
+
+
+}
+
+
+
+Console.WriteLine("Tryck På ENTER att sluta :)");
+Console.ReadLine();
